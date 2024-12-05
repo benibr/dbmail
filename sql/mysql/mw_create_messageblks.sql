5fa2ed193b2e8f181e29052b9eb9b3936a237f36
Beni <braunger@zib.de>
2024-12-05 20:43:18 +0100

    fixed mw_create_table messageblks

--- sql/mysql/mw_create_messageblks.sql
+++ sql/mysql/mw_create_messageblks.sql
@@ -1,12 +1,12 @@
 CREATE TABLE dbmail_messageblks (
     messageblk_idnr BIGINT AUTO_INCREMENT PRIMARY KEY,
-    physmessage_id BIGINT,
+    physmessage_id BIGINT(20) unsigned,
     messageblk LONGBLOB NOT NULL,
     blocksize BIGINT DEFAULT 0 NOT NULL,
     is_header SMALLINT DEFAULT 0 NOT NULL,
     FOREIGN KEY (physmessage_id) REFERENCES dbmail_physmessage(id)
         ON DELETE CASCADE ON UPDATE CASCADE
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_uca1400_ai_ci;
 
 CREATE INDEX dbmail_messageblks_physmessage_idx
     ON dbmail_messageblks(physmessage_id);
